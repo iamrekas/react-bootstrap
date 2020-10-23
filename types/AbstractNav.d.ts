721c955be58caff0fe4ffd2fab5b4869849a6129
Rekas <aure369@gmail.com>
2020-10-23 10:57:59 +0300

    prepare to test

---
+++ types/AbstractNav.d.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BsPrefixRefForwardingComponent } from './helpers';
+interface AbstractNavProps {
+    activeKey?: any;
+    as?: React.ElementType;
+    getControlledId?: any;
+    getControllerId?: any;
+    onKeyDown?: any;
+    onSelect?: any;
+    parentOnSelect?: any;
+    role?: string;
+    disableFocus?: boolean;
+}
+declare type AbstractNav = BsPrefixRefForwardingComponent<'ul', AbstractNavProps>;
+declare const AbstractNav: AbstractNav;
+export default AbstractNav;
